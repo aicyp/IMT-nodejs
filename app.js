1db5b25cbdaaa7a9097d2b88f2abf6b01333c2a1
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-01-08 10:43:51 +0100

    end tp3

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const contactFile = process.env.npm_package_config_contacts;
 const fs = require('fs');
 const commander = require('commander');
+const shortid = require('shortid');
 
 commander
-  .command('list').description('output a list of contact').action(() => {
+  .command('list')
+  .description('output a list of contact')
+  .action(() => {
     getListOfContact((data) => data.forEach(contact => {
       console.log(contact.lastName.toUpperCase() + " " + contact.firstName)
     }));
-  })
-  .command('add <firstName> <lastName>').description('adds a new contact to the list of contact').action(() => {
-    
-  })
-  .command('remove <id>').description('removes a contact of the list').action(() => {
+  });
 
+commander
+  .command('add <firstName> <lastName>')
+  .description('adds a new contact to the list of contact')
+  .action((firstName, lastName) => {
+    getListOfContact((data) => {
+      let item = { "id":shortid.generate(), "lastName":lastName, "firstName":firstName};
+      data.push(item);
+      overwriteListOfContact(data);
+    });
+  });
+
+commander
+  .command('remove <id>')
+  .description('removes a contact of the list')
+  .action((id) => {
+    getListOfContact((data) => {
+      overwriteListOfContact(data.filter(contact => contact.id !== id));
+    });
   });
 
 commander.parse(process.argv);
@@ -26,4 +43,15 @@ function getListOfContact(callback) {
 
     callback(JSON.parse(data));
   });
+}
+
+// overwrites the list of contact with a new contact
+function overwriteListOfContact(data) {
+  let dataToWrite = JSON.stringify(data, null, '\t');
+
+  fs.writeFile(contactFile, dataToWrite, (err) => {
+    if (err) throw err;
+
+    console.log('Contact file has been updated');
+  })
 }
