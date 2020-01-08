b20eeb777cdd359f48258a448673f0ce2175e413
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-01-08 09:44:13 +0100

    end tp1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 const contactFile = process.env.npm_package_config_contacts;
+const fs = require('fs');
+const commander = require('commander');
+
+commander.command('list').description('output a list of contact').action(function () {
+  let contactList = [];
+
+  fs.readFile(contactFile, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    contactList = JSON.parse(data);
+    contactList.forEach(contact => {
+      console.log(contact.lastName.toUpperCase() + " " + contact.firstName)
+    });
+  });
+});
+
+commander.parse(process.argv);
